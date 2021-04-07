81e91b4aa2ba79dc1c6523ba5d04770cd984ff07
Alec Lee <alec.lee15@gmail.com>
2021-04-07 15:09:47 -0700

    run linter

--- assets/webpack/index.js
+++ assets/webpack/index.js
@@ -3,17 +3,19 @@ import siteCard from "./templates/siteCard.handlebars";
 window.addEventListener("load", () => load());
 
 const initMap = () => {
-  mapboxgl.accessToken = 'pk.eyJ1IjoiY2FsbHRoZXNob3RzIiwiYSI6ImNrbjZoMmlsNjBlMDQydXA2MXNmZWQwOGoifQ.rirOl_C4pftVf9LgxW5EGw';
-  var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/calltheshots/ckn6plmc90jme17pqc65d55ld?optimize=true',
+  mapboxgl.accessToken =
+    "pk.eyJ1IjoiY2FsbHRoZXNob3RzIiwiYSI6ImNrbjZoMmlsNjBlMDQydXA2MXNmZWQwOGoifQ.rirOl_C4pftVf9LgxW5EGw";
+  const map = new mapboxgl.Map({
+    container: "map",
+    style:
+      "mapbox://styles/calltheshots/ckn6plmc90jme17pqc65d55ld?optimize=true",
     center: [-98, 40], // starting position [lng, lat]
-    zoom: 3 // starting zoom
+    zoom: 3, // starting zoom
   });
 
-  map.on('click', 'jesse', function(e) {
-    var coordinates = e.features[0].geometry.coordinates.slice();
-    var description = JSON.stringify(e.features[0].properties);
+  map.on("click", "jesse", function (e) {
+    const coordinates = e.features[0].geometry.coordinates.slice();
+    const description = JSON.stringify(e.features[0].properties);
 
     // Ensure that if the map is zoomed out such that multiple
     // copies of the feature are visible, the popup appears
@@ -22,30 +24,25 @@ const initMap = () => {
       coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
     }
 
-      new mapboxgl.Popup()
-        .setLngLat(coordinates)
-        .setHTML(description)
-        .addTo(map);
+    new mapboxgl.Popup().setLngLat(coordinates).setHTML(description).addTo(map);
   });
   // Change the cursor to a pointer when the mouse is over the places layer.
-  map.on('mouseenter', 'jesse', function () {
-    map.getCanvas().style.cursor = 'pointer';
+  map.on("mouseenter", "jesse", function () {
+    map.getCanvas().style.cursor = "pointer";
   });
   // Change it back to a pointer when it leaves.
-  map.on('mouseleave', 'jesse', function () {
-    map.getCanvas().style.cursor = '';
+  map.on("mouseleave", "jesse", function () {
+    map.getCanvas().style.cursor = "";
   });
 };
 
 const initCards = () => {
-    const range = document
-    .createRange()
-    .createContextualFragment(siteCard({}));
+  const range = document.createRange().createContextualFragment(siteCard({}));
 
-    document.getElementById("cards").appendChild(range)
+  document.getElementById("cards").appendChild(range);
 };
 
-const load = () => { 
-    initMap();
-    initCards();
-}
+const load = () => {
+  initMap();
+  initCards();
+};
