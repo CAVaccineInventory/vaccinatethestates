bd3b82c9d8f9709753b207e4f3050e8c811717a6
Alec Lee <alec.lee15@gmail.com>
2021-04-08 13:55:54 -0700

    lint

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,17 @@
 module.exports = {
-    purge: {
-      content: [
-        "./**/*.html",
-        "./**/*.md",
-        "./**/*.yml",
-        "./**/*.handlebars",
-      ],
-    },
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-      extend: {},
-    },
-    plugins: [
-      require("tailwindcss-rtl"),
+  purge: {
+    content: [
+      "./**/*.html",
+      "./**/*.md",
+      "./**/*.yml",
+      "./**/*.handlebars",
     ],
-  };
-  
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("tailwindcss-rtl"),
+  ],
+};
