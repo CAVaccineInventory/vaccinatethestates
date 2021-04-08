45c0fff395f939d60deae809dff99607037ec7e4
Alec Lee <alec.lee15@gmail.com>
2021-04-08 12:09:05 -0700

    Mostly correct styled header  (#3)
    * header css based on vaccinate ca
    
    * keep marquee because why not

---
+++ webpack/main.js
@@ -0,0 +1,27 @@
+window.addEventListener("load", () => {
+  initMobileMenu();
+});
+
+const initMobileMenu = () => {
+  const mobileMenuActivator = document.querySelector(
+    ".js-mobile-menu-activator"
+  );
+  const mobileMenuDeactivator = document.querySelector(
+    ".js-mobile-menu-deactivator"
+  );
+  const mobileMenu = document.querySelector(".js-mobile-menu");
+  document
+    .querySelector(".js-mobile-menu-button")
+    .addEventListener("click", (e) => {
+      mobileMenuActivator.classList.toggle("hidden");
+      mobileMenuActivator.classList.toggle("block");
+
+      mobileMenuDeactivator.classList.toggle("hidden");
+      mobileMenuDeactivator.classList.toggle("block");
+
+      mobileMenu.classList.toggle("hidden");
+      mobileMenu.classList.toggle("block");
+
+      e.preventDefault();
+    });
+};
