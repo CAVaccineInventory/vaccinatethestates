2a2b9daaef9dc5bfdeceec938dba5a19113934fc
Mike Skalnik <mike@skalnik.com>
2021-04-21 14:53:12 -0700

    Close all other `<details>` 📕

--- webpack/near-me.js
+++ webpack/near-me.js
@@ -149,6 +149,20 @@ const renderCardsFromMap = () => {
 
     cards.appendChild(range);
   });
+
+  // Ensure only one card can be open at a time
+  // From https://stackoverflow.com/questions/16751345/automatically-close-all-the-other-details-tags-after-opening-a-specific-detai
+  const details = document.querySelectorAll("details");
+
+  details.forEach((targetDetail) => {
+    targetDetail.addEventListener("click", () => {
+      details.forEach((detail) => {
+        if (detail !== targetDetail) {
+          detail.removeAttribute("open");
+        }
+      });
+    });
+  });
 };
 
 const getUniqueFeatures = (array) => {
