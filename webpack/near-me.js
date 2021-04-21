83fd45481729dddf1ecc43fd8906b6233623b003
Mike Skalnik <mike@skalnik.com>
2021-04-21 10:36:51 -0700

    Some small card improvements 😈
    - Smaller text in a few places
    - Feedback link works
    - More translation layer

--- webpack/near-me.js
+++ webpack/near-me.js
@@ -160,7 +160,9 @@ const renderCardsFromMap = () => {
       moreInfo: moreInfo,
       website: properties["website"],
       phoneNumber: properties["phone_number"],
-      appointmentDetails: properties["appointment_details"],
+      appointmentDetails: properties["appointment_details"]
+        ? markdownify(properties["appointment_details"])
+        : null,
       notes: properties["public_notes"]
         ? markdownify(properties["public_notes"])
         : null,
