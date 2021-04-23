9c3d9a3feea0bfcbe227ad02fd5a0a5fa3bd2312
Mike Skalnik <mike@skalnik.com>
2021-04-23 10:06:05 -0700

    Add new copy (#125)
    * New home page copy ©️
    
    * New about us copy 📄
    
    * Small about us tweaks 🕛️
    
    * No press kit yet 🎁
    
    * Woops, forgot this data 🧈
    
    * New volunteer form 🌋
    
    * Link to `/volunteer` ⚡️

--- webpack/about-us.js
+++ webpack/about-us.js
@@ -13,4 +13,26 @@ window.addEventListener("load", () => {
     },
     false
   );
+
+  const peopleElements = [...document.querySelectorAll("#js-people-list a")];
+  const peopleListElement = document.getElementById("js-people-list");
+
+  shuffleArray(peopleElements);
+  peopleListElement.innerHTML = "";
+  for (let i = 0; i < peopleElements.length; ++i) {
+    const personElement = peopleElements[i];
+
+    peopleListElement.insertBefore(personElement, null);
+    if (i !== peopleElements.length - 1) {
+      const separatorNode = document.createTextNode(", ");
+      peopleListElement.insertBefore(separatorNode, null);
+    }
+  }
 });
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
