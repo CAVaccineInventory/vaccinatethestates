cd264d977995036def772c9fcf1f55803abfe735
Alec Lee <alec.lee15@gmail.com>
2021-04-27 16:14:49 -0700

    ux improvement if using query params for embed

--- webpack/embed.js
+++ webpack/embed.js
@@ -1,9 +1,13 @@
 import { initSearch } from "./search.js";
 import { initMap, moveMap } from "./near-me.js";
+import { toggleVisibility } from "./utils/dom.js";
 
 window.addEventListener("load", () => load());
 
 const load = () => {
+  // if url params present, hide zoomed out view to avoid flash
+  toggleVisibility(document.querySelector("#zoomed_out_view"), !window.location.search);
+
   initMap();
   initSearch(
     {
