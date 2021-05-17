153f1623b12668717d3ff4f4052e27bceb19153c
Alec Lee <alec.lee15@gmail.com>
2021-05-17 14:04:45 -0700

    move page js to the page dir (#209)

--- webpack/pages/embed.js
+++ webpack/pages/embed.js
@@ -1,6 +1,6 @@
-import { initSearch } from "./search.js";
-import { initMap, moveMap } from "./near-me.js";
-import { toggleVisibility } from "./utils/dom.js";
+import { initSearch } from "../search.js";
+import { initMap, moveMap } from "../near-me.js";
+import { toggleVisibility } from "../utils/dom.js";
 
 window.addEventListener("load", () => load());
 
